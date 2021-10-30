88d967ad6b558d395419873b16a10e5a6c493560
Simon Mayerhofer <git@maysi.de>
2021-10-30 15:50:57 +0200

    Delete influx bucket to delete everything

--- influx_importer.py
+++ influx_importer.py
@@ -27,17 +27,28 @@ class InfluxImporter():
         self.client.close()
 
     def deleteExistingData(self):
-        delete_api = self.client.delete_api()
-        start = "1970-01-01T00:00:00Z"
-        stop = "2200-01-01T00:00:00Z"
+        print("delete bucket " + os.environ['INFLUXDB_BUCKET'])
+        buckets_api = self.client.buckets_api()
+        bucket = buckets_api.find_bucket_by_name(os.environ['INFLUXDB_BUCKET'])
+        buckets_api.delete_bucket(bucket)
 
-        delete_api.delete(
-            start,
-            stop,
-            '_measurement="' + self.measurement + '"',
-            bucket=os.environ['INFLUXDB_BUCKET'],
+        print("create bucket again")
+        buckets_api.create_bucket(
+            bucket_name=os.environ['INFLUXDB_BUCKET'],
             org=os.environ['INFLUXDB_ORG']
         )
+
+        #delete_api = self.client.delete_api()
+        #start = "1970-01-01T00:00:00Z"
+        #stop = "2200-01-01T00:00:00Z"
+
+        #delete_api.delete(
+        #    start,
+        #    stop,
+        #    '_measurement="' + self.measurement + '"',
+        #    bucket=os.environ['INFLUXDB_BUCKET'],
+        #    org=os.environ['INFLUXDB_ORG']
+        #)
 
     def startImport(self):
         dataFrame = pandas.read_csv(self.filepath, sep=";", skiprows=1, decimal=",", encoding="utf-8")
